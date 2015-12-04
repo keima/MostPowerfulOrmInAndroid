675e95fadf8e8e14c2d16584e4f7486f80f92f0f
Kouta Imanaka <keima@p-side.net>
2015-12-04 20:52:41 +0900

    サヨナラ！

--- app/src/androidTestSugarorm/java/net/pside/android/example/mostpowerfulorminandroid/SugarOrmApplication.java
+++
@@ -1,9 +0,0 @@
-package net.pside.android.example.mostpowerfulorminandroid;
-
-import com.orm.SugarApp;
-
-/**
- * Created by keima on 14/11/26.
- */
-public class SugarOrmApplication extends SugarApp {
-}
