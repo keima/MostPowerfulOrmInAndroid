cf6d91c398efa1326c625ff03bcd6867d97341da
Kouta Imanaka <keima@p-side.net>
2018-10-29 02:34:03 +0900

    Migrate JUnit4 on SlimRepo

--- appSlimrepo/src/androidTest/java/net/pside/android/example/mostpowerfulorminandroid/SlimrepoTest.java
+++ appSlimrepo/src/androidTest/java/net/pside/android/example/mostpowerfulorminandroid/SlimrepoTest.java
@@ -1,49 +1,60 @@
 package net.pside.android.example.mostpowerfulorminandroid;
 
-import android.util.Log;
-
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import com.slimgears.slimrepo.core.interfaces.RepositoryService;
 import com.slimgears.slimrepo.core.interfaces.conditions.Conditions;
-
-import net.pside.android.example.mostpowerfulorminandroid.library.OrmTestCase;
-import net.pside.android.example.mostpowerfulorminandroid.library.util.TimingLogger;
+import net.pside.android.example.mostpowerfulorminandroid.library.OrmBenchmark;
+import net.pside.android.example.mostpowerfulorminandroid.library.OrmBenchmarkRule;
 import net.pside.android.example.mostpowerfulorminandroid.model.GeneratedSimpleRepositoryService;
 import net.pside.android.example.mostpowerfulorminandroid.model.SimpleEntity;
 import net.pside.android.example.mostpowerfulorminandroid.model.SimpleRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
-public class SlimrepoTest extends OrmTestCase {
-    public static final String TAG = SlimrepoTest.class.getSimpleName();
-    public static final String DATABASE_NAME = SimpleRepository.DB_NAME_WITH_EXT;
+import static net.pside.android.example.mostpowerfulorminandroid.library.OrmBenchmarkRule.NUMBER_OF_INSERT_SINGLE;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class SlimrepoTest {
+
+    private static final String DATABASE_NAME = SimpleRepository.DB_NAME_WITH_EXT;
 
     private GeneratedSimpleRepositoryService mRepoService;
 
-    @Override
-    public String getDatabaseName() {
-        return DATABASE_NAME;
+    @Rule
+    public OrmBenchmarkRule rule = new OrmBenchmarkRule(
+            InstrumentationRegistry.getTargetContext(),
+            DATABASE_NAME
+    );
+
+    @Before
+    public void setUp() {
+        mRepoService = new GeneratedSimpleRepositoryService(
+                InstrumentationRegistry.getTargetContext()
+        );
     }
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        mRepoService = new GeneratedSimpleRepositoryService(mContext);
-    }
-
-    @Override
+    @Test
+    @OrmBenchmark(false)
     public void testSingleInsert() throws IOException {
         insert(false);
     }
 
-    @Override
+    @Test
+    @OrmBenchmark(true)
     public void testSingleBulkInsert() throws IOException {
         insert(true);
     }
 
     private void insert(boolean isBulkMode) throws IOException {
-        TimingLogger logger = new TimingLogger(TAG, MSG_LOGGER_INITIALIZE(TAG, isBulkMode));
+        rule.beginProfiling();
 
         if (isBulkMode) {
             mRepoService.update(new RepositoryService.UpdateAction<SimpleRepository>() {
@@ -56,15 +67,11 @@ public class SlimrepoTest extends OrmTestCase {
             });
         } else {
             for (int i = 1; i <= NUMBER_OF_INSERT_SINGLE; i++) {
-                try {
-                    mRepoService.simples().add(createSimple(i));
-                } catch (IOException e) {
-                    Log.e(TAG, "Catch!", e);
-                }
+                mRepoService.simples().add(createSimple(i));
             }
         }
 
-        logger.addSplit(MSG_LOGGER_SPLIT_INSERT);
+        rule.splitProfiling();
 
         List<SimpleEntity> simpleEntities = mRepoService.simples().query()
                 .where(Conditions.equals(SimpleEntity.BooleanValue, 1))
@@ -73,8 +80,7 @@ public class SlimrepoTest extends OrmTestCase {
 
         assertEquals(NUMBER_OF_INSERT_SINGLE / 2, simpleEntities.size());
 
-        logger.addSplit(MSG_LOGGER_SPLIT_SELECT);
-        logger.dumpToLog();
+        rule.endProfiling();
     }
 
     private SimpleEntity createSimple(int i) {
